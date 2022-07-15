dcdc326806f60a4203c6f33b3b5d5759f0630f68
Azat Minimullin <minimullin.azat@yandex.ru>
2022-07-16 01:22:14 +0300

    Добавил проверку на параллельность

--- Zadacha 43/Program.cs
+++ Zadacha 43/Program.cs
@@ -1,16 +1,26 @@
 ﻿/* Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
 заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем. */
 
-Console.Write("Введите k1: ");
-var b1 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Введите b1: ");
+var b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k1: ");
 var k1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите k2: ");
-var b2 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Введите b2: ");
+var b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
 var k2 = Convert.ToDouble(Console.ReadLine());
- 
-var x = (b2 - b1) / (k1 - k2);
-var y = k1 * x + b1;
- 
-Console.WriteLine($"Пересечение в точке: ({x};{y})"); 
+
+Check(b1, k1, b2, k2);
+void Check(double b1, double k1, double b2, double k2)
+{
+    if (k1  - k2 == 0)
+    {
+        Console.WriteLine("Прямые параллельны");
+    }
+    else
+    {
+        var x = (b2 - b1) / (k1 - k2);
+        var y = k1 * x + b1;
+        Console.WriteLine($"Пересечение в точке: ({x};{y})");
+    }
+}
